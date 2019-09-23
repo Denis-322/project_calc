672ea740415640cf011a8f25674e4d03f20870f2
Denis-322 <55235520+Denis-322@users.noreply.github.com>
2019-09-23 14:43:14 +0300

    qq

--- src/App.js
+++ src/App.js
@@ -5,28 +5,28 @@ class App extends Component {
     state = {
         result: ''
     };
-    buttonPressed(buttonName, value, value2) {
-        console.log(buttonName, value, value2);
+    buttonPressed(buttonName, value1, value2) {
+        console.log(buttonName, value1, value2);
         if (buttonName === 'plus') {
-            let numeral1 = this.state.value;
+            let numeral1 = this.state.value1;
             let numeral2 = this.state.value2;
             let result = parseFloat(numeral1 + numeral2);
             return (result)
 
         } else if (buttonName === 'minus') {
-            let numeral1 = parseFloat(value);
+            let numeral1 = parseFloat(value1);
             let numeral2 = parseFloat(value2);
             let result = parseFloat(numeral1 - numeral2);
             return (result)
 
         } else if (buttonName === 'umnozhenie') {
-            let numeral1 = parseFloat(value);
+            let numeral1 = parseFloat(value1);
             let numeral2 = parseFloat(value2);
             let result = parseFloat(numeral1 * numeral2);
             return (result)
 
         } else if (buttonName === 'delenie') {
-            let numeral1 = parseFloat(value);
+            let numeral1 = parseFloat(value1);
             let numeral2 = parseFloat(value2);
             let result = parseFloat(numeral1 / numeral2);
             return (result)
--- src/components/Numbers.js
+++ src/components/Numbers.js
@@ -1,30 +1,11 @@
 import React, { Component } from "react";
 
 export default class Numbers extends Component {
-    buttonPressed = e => {
-        this.props.buttonPressed(e.target.name);
-    }
-    handleChange(event) {
-        this.setState({ value: event.target.value });
-        }
-    handleChange2(event) {
-        this.setState({ value2: event.target.value });
-        }
-    constructor(props)
-     {
-        super(props);
-        this.state = {value: '',value2:''};
-         
-        this.handleChange = this.handleChange.bind(this);
-        this.handleChange2 = this.handleChange2.bind(this);
-
-        
-        }
     render() {
         return (
             <div>
-                <p>Первое число: <input type="number" value={this.state.value} onChange={this.handleChange} /></p>
-                <p>Второе число: <input type="number" value={this.state.value2} onChange={this.handleChange2} /></p>
+                <p>Первое число: <input type="number" value1={this.state.value1} onChange={this.handleChange} /></p>
+                <p>Второе число: <input type="number" value2={this.state.value2} onChange={this.handleChange2} /></p>
                 <button name="plus" onClick={this.buttonPressed}>Сложить </button>
                 <button name="minus" onClick={this.buttonPressed}>Вычесть </button>
                 <button name="umnozhenie" onClick={this.buttonPressed}>Умножить </button>
@@ -33,5 +14,24 @@ export default class Numbers extends Component {
             </div>
         );
     }
+    buttonPressed = e => {
+        this.props.buttonPressed(e.target.name);
+    }
+    handleChange(event) {
+        this.setState({ value1: event.target.value1 });
+        }
+    handleChange2(event) {
+        this.setState({ value2: event.target.value2 });
+        }
+    constructor(state)
+     {
+        super(state);
+        this.state = {value1: ``,value2:``};
+         
+        this.handleChange = this.handleChange.bind(this);
+        this.handleChange2 = this.handleChange2.bind(this);
+
+        
+        }
 
 }
